c98b1e3deff39a3638fa94b944da6f8e698f74d4
Suman Tiwari <zx.suman7@gmail.com>
2020-12-16 15:06:17 +0545

    First commit

---
+++ src/main/java/com/example/medo/MedoApplication.java
@@ -0,0 +1,13 @@
+package com.example.medo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/medo/MedoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
